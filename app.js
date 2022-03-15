69841fba8992f17241e0ccc093e075f3d48beeae
rayhankinan <rayhankinan@gmail.com>
2022-03-15 22:55:59 +0700

    Menambah upload gambar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Middleware
 require('dotenv').config()
+
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
@@ -7,20 +8,40 @@ const cors = require('cors')
 const mysql = require('mysql')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { format } = require('util')
+
+const Multer = require('multer')
+const { Storage } = require('@google-cloud/storage')
+const serviceKey = require('./keys.json')
 
 // Global Variable
 const app = express()
+
 const port = parseInt(process.env.PORT)
+const token = parseInt(process.env.JWT_TOKEN)
+const adminUsername = process.env.ADMIN_USERNAME
+const adminPassword = process.env.ADMIN_PASSWORD
+const maxSize = parseInt(process.env.MAX_SIZE)
+const checkURL = process.env.NAMA_NIM_URL
+
 const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
-const token = parseInt(process.env.JWT_TOKEN)
-const adminUsername = process.env.ADMIN_USERNAME
-const adminPassword = process.env.ADMIN_PASSWORD
-const geprekURL = 'https://cdn.jsdelivr.net/gh/mkamadeus'
+
+const storage = new Storage({
+    keyFilename: serviceKey,
+    projectId: 'exhibition-page'
+})
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: maxSize * 1024 * 1024
+    }
+})
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET)
 
 // Configuration
 app.use(bodyParser.urlencoded({
@@ -67,7 +88,7 @@ app.get('/', (req, res) => {
 
 // Admin
 app.get('/admin', (req, res) => {
-    const { body } = req.body
+    const { body } = req
 
     if (body?.username === adminUsername && body?.password) {
         bcrypt.compare(body.password, adminPassword, (hashError, hashResult) => {
@@ -91,6 +112,53 @@ app.get('/admin', (req, res) => {
 
 app.all('/admin/*', requireAdmin, (req, res, next) => {
     next()
+})
+
+app.post('/admin/upload', multer.single('file'), (req, res, next) => {
+    const { body, file } = req
+
+    if (body?.title && body?.name && body?.type) {
+        if (file) {
+            const blob = bucket.file(file.originalname)
+            const blobStream = blob.createWriteStream()
+
+            blobStream.on('error', (blobStreamError) => {
+                next(blobStreamError)
+            })
+
+            blobStream.on('finish', () => {
+                const publicURL = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`)
+
+                connection.query(
+                    'INSERT INTO Team (Title, Name, Type, Description, LinkToHeader) values (?, ?, ?, ?, ?)', 
+                    [body.title, body.name, body.type, body.description, publicURL], 
+                    (databaseError, databaseResults) => {
+                        if (databaseError) {
+                            res.sendStatus(500)
+                        } else {
+                            res.sendStatus(200)
+                        }
+                    }
+                )
+            })
+
+        } else {
+            connection.query(
+                'INSERT INTO Team (Title, Name, Type, Description) values (?, ?, ?, ?)', 
+                [body.title, body.name, body.type, body.description],
+                (databaseError, databaseResults) => {
+                    if (databaseError) {
+                        res.sendStatus(500)
+                    } else {
+                        res.sendStatus(200)
+                    }
+                }
+            )
+        }
+
+    } else {
+        res.sendStatus(400)
+    }
 })
 
 // User
---
+++ database.sql
@@ -0,0 +1,115 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: exhibition_page
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `exhibition_page`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `exhibition_page` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `exhibition_page`;
+
+--
+-- Table structure for table `picture`
+--
+
+DROP TABLE IF EXISTS `picture`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `picture` (
+  `IDPicture` int unsigned NOT NULL AUTO_INCREMENT,
+  `IDTeam` int unsigned NOT NULL,
+  `LinkToImage` varchar(512) NOT NULL,
+  PRIMARY KEY (`IDPicture`),
+  KEY `IDTeam` (`IDTeam`),
+  CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`IDTeam`) REFERENCES `team` (`IDTeam`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `picture`
+--
+
+LOCK TABLES `picture` WRITE;
+/*!40000 ALTER TABLE `picture` DISABLE KEYS */;
+/*!40000 ALTER TABLE `picture` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `team` (
+  `IDTeam` int unsigned NOT NULL AUTO_INCREMENT,
+  `Title` varchar(256) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  `Type` enum('Business Plan','Innovation Competition') NOT NULL,
+  `Description` longtext,
+  `LinkToHeader` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`IDTeam`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team`
+--
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voter`
+--
+
+DROP TABLE IF EXISTS `voter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `voter` (
+  `IDVoter` int unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(256) NOT NULL,
+  `IDStudent` varchar(8) NOT NULL,
+  `IDTeam` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`IDVoter`),
+  KEY `IDTeam` (`IDTeam`),
+  CONSTRAINT `voter_ibfk_1` FOREIGN KEY (`IDTeam`) REFERENCES `team` (`IDTeam`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voter`
+--
+
+LOCK TABLES `voter` WRITE;
+/*!40000 ALTER TABLE `voter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `voter` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-15 12:49:17
